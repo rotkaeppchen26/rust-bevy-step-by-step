2e2ff4c66c08e328c35e98a7fb1c43cdf9c0c2fc
rotkaeppchen26 <schoefl.josef@gmail.com>
2024-12-07 16:35:21 +0100

    Generate Chessboard

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(HelloPlugin)
-        .add_systems(Startup, setup)
-        .add_systems(Update, move_player)
+        .add_plugins(ChessPlugin)
         .run();
 }
 
+/***
+ * Introduction
+ * - Hello World!
+ * - Update Schedule
+ */
 #[derive(Component)]
 struct Person;
 
@@ -51,10 +54,25 @@ fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Na
     }
 }
 
+/***
+ * MovingCircle
+ * - Marker Components
+ * - Input Handling
+ * - Movement
+ */
 #[derive(Component)]
 struct Player;
 
-fn setup(
+pub struct MovingCirclePlugin;
+
+impl Plugin for MovingCirclePlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, player_setup);
+        app.add_systems(Update, move_player);
+    }
+}
+
+fn player_setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
@@ -93,6 +111,57 @@ fn move_player(
         }
         if 0.0 < direction.length() {
             transform.translation += MOVE_SPEED * direction.normalize();
+        }
+    }
+}
+
+/***
+ * Chess
+ * - Render Grid
+ * - Unit Selection
+ * - Win/Lose Condition
+ */
+
+pub struct ChessPlugin;
+
+impl Plugin for ChessPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, (chess_setup, create_chessboard));
+    }
+}
+
+fn chess_setup(mut commands: Commands) {
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_matrix(Mat4::from_rotation_translation(
+            Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
+            Vec3::new(-7., 20., 4.),
+        )),
+    ));
+    commands.spawn( (
+        PointLight { intensity: 3_000_000., ..Default::default() },
+        Transform::from_translation(Vec3::new(4.,8.,4.))
+    ));
+}
+
+fn create_chessboard(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {    
+    for i in 0..8 {
+        for j in 0..8 {
+            let mut color = Color::BLACK;
+
+            if (i+j+1) % 2 == 0 {
+                color = Color::WHITE;
+            }
+
+            commands.spawn((
+                Mesh3d(meshes.add(Plane3d::default().mesh().size(1., 1.))),
+                MeshMaterial3d(materials.add(color)),
+                Transform::from_translation(Vec3::new(i as f32, 0., j as f32))
+            ));
         }
     }
 }
