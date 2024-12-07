3fafa640a596f2171ee069c33026e3929b5c63af
rotkaeppchen26 <schoefl.josef@gmail.com>
2024-12-07 14:25:16 +0100

    Movement

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(HelloPlugin)
+        .add_systems(Startup, setup)
+        .add_systems(Update, move_player)
         .run();
 }
 
@@ -45,6 +47,52 @@ fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Na
     if timer.0.tick(time.delta()).just_finished() {
         for name in &query {
             println!("hello {}!", name.0);
+        }
+    }
+}
+
+#[derive(Component)]
+struct Player;
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2d::default());
+    commands.spawn((
+        Player,
+        Mesh2d(meshes.add(Circle { radius: 1.0 })),
+        MeshMaterial2d(materials.add(Color::WHITE)),
+        Transform {
+            translation: Vec3::new(0.0, 0.0, 0.0),
+            scale: Vec2::new(50.0, 50.0).extend(1.0),
+            ..Default::default()
+        },
+    ));
+}
+
+const MOVE_SPEED: f32 = 6.0;
+fn move_player(
+    mut transforms: Query<&mut Transform, With<Player>>,
+    keys: Res<ButtonInput<KeyCode>>,
+) {
+    for mut transform in transforms.iter_mut() {
+        let mut direction = Vec3::ZERO;
+        if keys.pressed(KeyCode::KeyW) {
+            direction.y += 1.0;
+        }
+        if keys.pressed(KeyCode::KeyS) {
+            direction.y -= 1.0;
+        }
+        if keys.pressed(KeyCode::KeyA) {
+            direction.x -= 1.0;
+        }
+        if keys.pressed(KeyCode::KeyD) {
+            direction.x += 1.0;
+        }
+        if 0.0 < direction.length() {
+            transform.translation += MOVE_SPEED * direction.normalize();
         }
     }
 }
